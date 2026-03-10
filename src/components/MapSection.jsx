70d2f257701dbdd83936aab20f2d04cb071f29bf
Faitorxd <walteroterotorres2003@gmail.com>
2026-03-10 16:09:53 -0500

    disable eslint hook deps

--- src/components/MapSection.jsx
+++ src/components/MapSection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef, useState } from 'react';
 import { Navigation, MapPin, X } from 'lucide-react';
 import useInView from '../hooks/useInView';
